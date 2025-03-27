a292b21a20819aaf9c49c802b73366312c86fc37
Brian Dang <dangggbrian@gmail.com>
2025-03-26 23:20:36 -0700

    Third Push

--- website_login.php
+++ website_login.php
@@ -3,7 +3,7 @@
 session_start();
 require_once 'config.php';
 
-if (isset($_POST[''])) {
+if (isset($_POST['register'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -17,6 +17,34 @@ if (isset($_POST[''])) {
         $conn->query("INSERT INTO users (name, email, password, role) VALUES ('$name','$email','$password','$role')")
     }
 
+    header("Location: index.php")
+    exit();
+}
+
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users['password']")
+    if (result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+
+            if ($user['role'] == 'admin') {
+                header("Location: admin_page.php")
+            } else {
+                header("Location: user_page.php")
+            }
+
+            exit();
+        }
+    }
+
+    $_SESSION['login_error'] = 'Incorrect email or password';
+    $_SESSION['active_form'] = 'login';
     header("Location: index.php")
     exit();
 }
